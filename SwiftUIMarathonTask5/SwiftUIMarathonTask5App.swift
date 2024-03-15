410f3709d8b28fc1fe585935cc31f267e96adbe2
justSmK <28645140+justSmK@users.noreply.github.com>
2024-03-15 12:28:18 +0300

    Init project

---
+++ SwiftUIMarathonTask5/SwiftUIMarathonTask5App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMarathonTask5App.swift
+//  SwiftUIMarathonTask5
+//
+//  Created by Sergei Semko on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMarathonTask5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
